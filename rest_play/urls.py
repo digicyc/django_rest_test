0513d7c5fbdb481228b3f4618aad9720e4118859
AaronA <digicyc@gmail.com>
2013-07-08 12:28:21 -0600

    Some mild cleanup

--- rest_play/urls.py
+++ rest_play/urls.py
@@ -8,6 +8,4 @@ urlpatterns = patterns('',
     url(r'^', include('snippets.urls')),
     url(r'^oauth2/', include('provider.oauth2.urls', namespace='oauth2')),
     url(r'^restadmin/', include(admin.site.urls)),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
